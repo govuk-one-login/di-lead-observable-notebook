2a50f2b256d24300d1fb8ab604106791f8bd24b7
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2024-07-03 13:09:07 +0100

    Add a data loader to hit the github API and ETL some basic repo data
    Uses octokit, was kinda fiddly, do wonder if it's not just easier to
    drop octo and go directly for API calls, only hesitation I had was
    pagination parsing, which I was too lazy to do without octokit.

---
+++ src/data/github/repositories.js
@@ -0,0 +1,55 @@
+import "dotenv/config";
+import fs from "fs";
+import { Octokit } from "@octokit/core";
+import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
+import { paginateRest } from "@octokit/plugin-paginate-rest";
+
+const { GITHUB_TOKEN } = process.env;  
+const ORG = "govuk-one-login";
+
+const RestOcto = Octokit.plugin(restEndpointMethods, paginateRest);
+const octokit = new RestOcto({ auth: GITHUB_TOKEN });
+
+// Function to fetch repositories for the given organization using pagination and iterator
+const fetchRepositories = async () => {
+  const iterator = octokit.paginate.iterator(octokit.rest.repos.listForOrg.endpoint.merge({
+    org: ORG,
+    per_page: 100,
+  }));
+
+  const allRepos = [];
+
+  for await (const { data: repos } of iterator) {
+    const reposWithProperties = await Promise.all(repos.map(async (repo) => {
+      const properties = await octokit.rest.repos.getCustomPropertiesValues({ owner: ORG, repo: repo.name });
+      // const sbom = await octokit.rest.dependencyGraph.exportSbom({ owner: ORG, repo: repo.name });
+      return {
+        name: repo.name,
+        description: repo.description,
+        private: repo.private,
+        archived: repo.archived,
+        pod: properties.data.filter(item => item.property_name === "pod")[0].value
+        // sbom: sbom
+      };
+    }));
+
+    allRepos.push(...reposWithProperties);
+  }
+
+  return allRepos;
+};
+
+// Function to write data to a file
+const writeToFile = (filename, data) => {
+  fs.writeFileSync(filename, JSON.stringify(data, null, 2));
+};
+
+// Main function to run the data loader
+const main = async () => {
+  const repos = await fetchRepositories();
+  writeToFile('src/data/github/repositories.json', repos);
+  console.log('Repositories data written to src/data/github/repositories.json');
+};
+
+// Run the main function
+main();
